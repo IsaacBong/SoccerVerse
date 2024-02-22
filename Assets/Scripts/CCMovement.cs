5e93958b28fbd40c5f90b5e02abc51d327eed085
IsaacBong <141281623+IsaacBong@users.noreply.github.com>
2024-02-23 08:05:29 +1100

    2ndScene&PostProcessing

---
+++ Assets/Scripts/CCMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCMovement : MonoBehaviour
+{
+    [Header("Movement Values")]
+    [SerializeField] float movementSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float gravityForce;
+    [SerializeField] float jumpForce;
+
+    [Header("Components")]
+    [SerializeField] CharacterController cc;
+    [SerializeField] Animator anim;
+    [SerializeField] Camera cam;
+
+    [Header("Targetting")]
+    public Transform target;
+    public bool shouldLook;
+
+    Vector3 movementDirection;
+    Vector3 playerVelocity;
+    public bool groundedPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //process gravity: check if grounded and reduce velocity if so
+
+        //process player inputs
+        float h = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //get camera directions
+        Vector3 camX = cam.transform.right;
+        Vector3 camZ = Vector3.Cross(camX, Vector3.up);
+
+        //if moving, combine camera directions and inputs then move
+        if (h != 0 || z != 0) 
+        {
+            movementDirection = camX * h + (camZ * z);
+            movementDirection.Normalize();
+
+            cc.Move(movementDirection * movementSpeed * Time.deltaTime);
+        }
+        //if not locked onto target, determine rotation towards walking direction
+
+        //if locked onto target, determine rotation towards target
+
+        //find direction for animation based on movement relative to faceing
+
+        //process gravity: apply downward motion
+    }
+}
--- Assets/Scripts/Goall.cs
+++ Assets/Scripts/Goall.cs
@@ -6,12 +6,17 @@ using UnityEngine.Events;
 public class Goall : MonoBehaviour
 {
     public UnityEvent score;
+    private GameObject cam;
+    private float lastValue;
+    private LensDistortion Distortion;
+    public VolumeProfile profile;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Ball")
         {
             score.Invoke();
+            profile.TryGet<LensDistortion>(out Distortion);
         }
     }
 }
