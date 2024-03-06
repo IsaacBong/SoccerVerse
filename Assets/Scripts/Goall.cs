b5c65cfc3ebb81b7ed71a0cfa43df7f133a336dc
IsaacBong <141281623+IsaacBong@users.noreply.github.com>
2024-03-07 09:27:49 +1100

    Reset

--- Assets/Scripts/Goall.cs
+++ Assets/Scripts/Goall.cs
@@ -2,32 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-
-//using UnityEngine.Rendering.PostProcessing;
 
 public class Goall : MonoBehaviour
 {
     public UnityEvent score;
-    private GameObject cam;
-    private float lastValue;
-    private LensDistortion Distortion;
-    public Volume volume;
-
-    private void Start()
-    {
-       volume.profile.TryGet<LensDistortion>(out Distortion);
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Ball")
         {
             score.Invoke();
-            
-            Distortion.intensity.value = intensity;
-
         }
     }
 }
