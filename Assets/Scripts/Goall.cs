4e598901add0dd4beee61395fab1ddef4496fd51
IsaacBong <141281623+IsaacBong@users.noreply.github.com>
2024-03-07 13:17:26 +1100

    Reset

--- Assets/Scripts/Goall.cs
+++ Assets/Scripts/Goall.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Goall : MonoBehaviour
 {
     public UnityEvent score;
+    public Volume volume
 
+    public void Start()
+    {
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Ball")
