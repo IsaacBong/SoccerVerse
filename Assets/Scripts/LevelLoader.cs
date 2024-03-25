c66582703716c5b69a0f8c5922910454c5f31118
IsaacBong <141281623+IsaacBong@users.noreply.github.com>
2024-03-26 10:58:51 +1100

    HotFix

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "player")LoadNextLevel();
+    }
+}
